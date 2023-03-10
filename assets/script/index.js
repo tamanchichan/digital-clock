511f28b2b231d95cbb428f7d6d0a31c082c647c7
tamanchichan <tamanchichan@gmail.com>
2023-03-10 10:12:28 -0600

    function added

--- assets/script/index.js
+++ assets/script/index.js
@@ -5,3 +5,11 @@ const digitalClock = document.querySelector('#digitalClock');
 setInterval(() => {
   return digitalClock.innerText = new Date().toLocaleTimeString();
 }, 1000);
+
+const alarm = document.querySelector('#alarm');
+const input = document.querySelector('.input');
+const button = document.querySelector('.button');
+
+button.addEventListener('click', () => {
+  alarm.append(input.value);
+});
