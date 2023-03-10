d8d1609be08c09de98fe072807abea18cec827b3
tamanchichan <tamanchichan@gmail.com>
2023-03-10 10:56:36 -0600

    validation added

--- assets/script/index.js
+++ assets/script/index.js
@@ -9,7 +9,23 @@ setInterval(() => {
 const alarm = document.querySelector('#alarm');
 const input = document.querySelector('.input');
 const button = document.querySelector('.button');
+const output = document.querySelector('#output');
+
+function hasColon() {
+  if (input.value.includes(':') && input.value.indexOf(':') === 2) {
+    return true;
+  }
+  
+  else {
+    return false;
+  }
+}
 
 button.addEventListener('click', () => {
-  alarm.append(input.value);
+  if (hasColon()) {
+    alarm.innerText = input.value;
+    input.value = '';
+  } else {
+    output.innerText = 'Please follow the example below: 00:00';
+  }
 });
