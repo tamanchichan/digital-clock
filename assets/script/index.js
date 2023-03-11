d88630714fece7281799953d2b4eabbe1d48c030
tamanchichan <tamanchichan@gmail.com>
2023-03-10 23:17:33 -0600

    alarm function added

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,34 +1,61 @@
 'use strict';
 
 const digitalClock = document.querySelector('#digitalClock');
-
-let clock = setInterval(() => {
-  digitalClock.innerText = new Date().toLocaleTimeString();
-}, 1000);
-
 const alarm = document.querySelector('#alarm');
 const input = document.querySelector('.input');
 const button = document.querySelector('.button');
 const output = document.querySelector('#output');
 
-function hasColon() {
-  if (input.value.includes(':') && input.value.indexOf(':') === 2) {
-    return true;
+const alarmSound = new Audio('./assets/audio/alarm.mp3');
+alarmSound.type = 'audio/mp3';
+
+function clock() {
+  let hours = new Date().getHours().toString().padStart(2, '0');
+  let minutes = new Date().getMinutes().toString().padStart(2, '0');
+  let seconds = new Date().getSeconds().toString().padStart(2, '0');
+  let time = `${hours}:${minutes}:${seconds}`;
+  
+  setInterval(
+    () => {
+      digitalClock.innerText = clock();
+    }, 1000
+  );
+  
+  if (alarm.innerText === `${hours}:${minutes}`) {
+    playAlarm();
+    // NOT WORKING! IT IS GLITCHING
+    // setTimeout(
+    //   () => {
+    //     stopAlarm();
+    //   }, 5000
+    // )
   }
   
-  else {
+  return time;
+}
+
+clock();
+
+function hasColon() {
+  if (input.value.includes (':') && input.value.indexOf(':') === 2) {
+    return true;
+  } else {
     return false;
   }
+}
+
+function playAlarm() {
+  alarmSound.play();
+}
+
+function stopAlarm() {
+  alarmSound.pause();
 }
 
 button.addEventListener('click', () => {
   if (hasColon()) {
     alarm.innerText = input.value;
-    input.value = '';
   } else {
-    output.innerText = 'Please follow the example below: 00:00';
+    output.innerText = 'Please follow the example below: hh:mm (12:34)'
   }
 });
-
-const alarmSound = new Audio('./assets/audio/alarm.mp3');
-alarmSound.type = 'audio/mp3';
